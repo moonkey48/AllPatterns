bca8f5bc4681db19a7ade44c842f0be208993143
moonkey48 <moonkey48@icloud.com>
2024-10-31 00:21:38 +0900

    setting: create project

---
+++ AllPatterns/Source/Core/AllPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllPatternsApp.swift
+//  AllPatterns
+//
+//  Created by Austin's Macbook Pro M3 on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct AllPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
